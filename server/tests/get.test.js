37e06e4e2739befb8a83fce2e7529e6c9dab5311
Leven Rochana <levrotech.googl@gmail.com>
2025-06-04 21:28:08 +0530

    reformat(server): configure tests to use async/await for callbacks

--- server/tests/get.test.js
+++ server/tests/get.test.js
@@ -5,17 +5,13 @@ import { expect } from 'chai'
 import app from '../app.js'
 
 describe('GET /', () => {
-  it('should return a status code of 200 and an empty array if no todos exist', function (done) {
-    request(app)
-      .get('/todo')
-      .end(function (err, res) {
-        if (err) return done(err)
-        expect(res.statusCode).to.be.equal(200)
-        expect(res.body).to.have.property('success')
-        expect(res.body.success).to.be.equal(true)
-        expect(res.body).to.have.property('data')
-        expect(res.body.data).to.be.a('array')
-        done()
-      })
+  it('should return a status code of 200 and an empty array if no todos exist', async function () {
+    const response = await request(app).get('/todo')
+
+    expect(response.statusCode).to.be.equal(200)
+    expect(response.body).to.have.property('success')
+    expect(response.body.success).to.be.equal(true)
+    expect(response.body).to.have.property('data')
+    expect(response.body.data).to.be.a('array')
   })
 })

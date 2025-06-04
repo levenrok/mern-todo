06e894530158ae2db2b5e4af639edf86fb7ea18d
Leven Rochana <levrotech.googl@gmail.com>
2025-06-04 21:15:46 +0530

    feat(server): implement 'GET /' endpoints - Fetches all documents from the 'Todo' collection. - Responds with a 200 status code and a JSON object containing a data key holding the retrieved todos.

--- server/routes/todo.route.js
+++ server/routes/todo.route.js
@@ -1,7 +1,16 @@
 import { Router } from 'express'
 
+import Todo from '../models/todo.model.js'
+
 const todoRouter = Router()
 
-todoRouter.get('/', async (req, res) => {})
+todoRouter.get('/', async (req, res) => {
+  const todos = await Todo.find()
+
+  res.status(200).json({
+    success: true,
+    data: todos
+  })
+})
 
 export default todoRouter

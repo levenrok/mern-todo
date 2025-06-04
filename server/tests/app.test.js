bee122615640e1ce1a6d3cf707d335b46de4b938
Leven Rochana <levrotech.googl@gmail.com>
2025-06-04 17:06:56 +0530

    test(server): change the testing framework to 'mocha'

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -1,24 +1,27 @@
 import request from 'supertest'
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
 
-import app from '../app.js'
+import app from '../app.js' // Ensure this path is correct and 'app' is exported from it
 
-// eslint-disable-next-line no-undef
 describe('GET /', () => {
-// eslint-disable-next-line no-undef
-  it('should get a status code of 200', async () => {
-    // eslint-disable-next-line no-undef
-    const response = await request(app).get('/')
-
-    // eslint-disable-next-line no-undef
-    expect(response.statusCode).toBe(200)
+  it('should get a status code of 200', function (done) {
+    request(app)
+      .get('/')
+      .end(function (err, res) {
+        if (err) return done(err)
+        expect(res.statusCode).to.be.equal(200)
+        done()
+      })
   })
 
-  // eslint-disable-next-line no-undef
-  it("should contain 'Hello, World' in the text", async () => {
-    // eslint-disable-next-line no-undef
-    const response = await request(app).get('/')
-
-    // eslint-disable-next-line no-undef
-    expect(response.text).toContain('Hello, World')
+  it("should contain 'Hello, World' in the text", function (done) {
+    request(app)
+      .get('/')
+      .end(function (err, res) {
+        if (err) return done(err)
+        expect(res.text).to.contain('Hello, World')
+        done()
+      })
   })
 })
